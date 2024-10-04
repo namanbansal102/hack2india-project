1dc58670c9cb8f2453fe751f8f522e07780b8e8a
Naman Bansal <namanbansal102@gmail.com>
2024-10-04 21:14:40 +0530

    Added Accentrinicity UI

---
+++ src/app/mypage/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AnimatedModalDemo } from './AnimatedModalDemo'
+
+const page = () => {
+  return (
+    <div><AnimatedModalDemo></AnimatedModalDemo></div>
+  )
+}
+
+export default page
---
+++ src/app/ourpage/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ThreeDCardDemo } from './ThreedCardDemo'
+const page = () => {
+  return (
+    <div>
+<ThreeDCardDemo></ThreeDCardDemo>
+    </div>
+  )
+}
+
+export default page
