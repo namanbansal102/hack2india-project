f95ec93f2184cb4d4fbf9fd59f196ac777217dd6
Naman Bansal <namanbansal102@gmail.com>
2024-10-04 21:23:15 +0530

    Fixed Bug

--- src/app/ourpage/page.jsx
+++ src/app/ourpage/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ThreeDCardDemo } from './ThreedCardDemo'
+import { ThreeDCardDemo } from './ThreedCardDemo.js'
 import {Avatar} from "@nextui-org/react";
 const page = () => {
   return (
