f5db759352e07341adf4081f1d6b606e52cfebb3
Naman Bansal <namanbansal102@gmail.com>
2024-10-06 21:50:46 +0530

    Fixed bug in deployemnt

--- src/app/ourpage/page.js
+++ src/app/ourpage/page.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { ThreeDCardDemo } from './ThreedCardDemo.jsx'
 import {Avatar} from "@nextui-org/react";
 const page = () => {
   return (
@@ -7,7 +6,6 @@ const page = () => {
         
         <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
 
-<ThreeDCardDemo></ThreeDCardDemo>
     </div>
   )
 }
